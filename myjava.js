61b58f3a04133c3664247e350b54a4cb9d0c23de
lengochuyen25 <huyenln.hvnh@gmail.com>
2019-08-13 10:41:58 +0700

    a

---
+++ myjava.js
@@ -0,0 +1,9 @@
+let value = prompt("Enter a number: ");
+let x = [-3, 5, 1, 3, 2, 10];
+for (i = 0; i < x.length; i++) {
+    if (value == x[i]) {
+        alert("Value " + x[i] + " found at " + i);
+    } else {
+    }
+    alert("Value didn't find!")
+}
